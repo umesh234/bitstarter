e404a81acd1928e6fca7b5717ee7d871f7f30455
ukumar <ukumar@gmail.com>
2013-07-08 00:05:21 +0000

    updated web.js to read index.html

--- web.js
+++ web.js
@@ -1,12 +1,15 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  Buffer content = fs.readFileSync('index.html' );
+  response.send(content.toString());
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
